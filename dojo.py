5d3ca8ee84d27634a82bc12eccba2ab04284dc19
Andrea Crotti <andrea.crotti.0@gmail.com>
2013-01-07 20:17:31 +0000

    add message class

--- dojo.py
+++ dojo.py
@@ -3,6 +3,26 @@ __metaclass__ = type
 BROADCAST_ADDR = "10.145.2.255:10001"
 
 import multiprocessing
+DEL = "::"
+
+
+class Msg:
+    def __init__(self, src, dest, content):
+        self.src = src
+        self.dest = dest
+        self.content = content
+        self.passed_from = []
+
+    @classmethod
+    def from_string(cls, st_repr):
+        src, dest, content, passed_from = st_repr.split('::')
+        return cls(Msg(src, dest, content, passed_from.split(',')))
+
+    def to_string(self):
+        return str(self)
+
+    def __str__(self):
+        return "%s::%s::%s::%s" % (self.src, self.dest, self.content, ','.join(self.passed_from))
 
 
 class Node:
@@ -11,7 +31,13 @@ class Node:
         self.neighbours = []
 
     def route(self, msg):
-        pass
+        if msg.dest == self.port:
+            print("Received message %s" % str(msg))
+        elif msg.dest in self.neighbours:
+            msg.passed_from.append(self.port)
+            self.send(msg, msg.dest)
+        else:
+            not_seen = [x for x in self.neighbours if not x in msg.passed_from]
 
     def check(self):
         pass
